b3f2312cffd42f8214ab0afbdcf122f0d5886c70
JV-VM <joaovictoroliveiradourado2007@gmail.com>
2024-11-30 17:32:07 -0300

    chore: apagando target

--- backend/app/target/debug/deps/ident_case-6491157a9dced91d.d
+++
@@ -1,7 +0,0 @@
-C:\Users\joaov\Documents\waifu_moe\backend\target\debug\deps\libident_case-6491157a9dced91d.rmeta: C:\Users\joaov\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ident_case-1.0.1\src\lib.rs
-
-C:\Users\joaov\Documents\waifu_moe\backend\target\debug\deps\libident_case-6491157a9dced91d.rlib: C:\Users\joaov\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ident_case-1.0.1\src\lib.rs
-
-C:\Users\joaov\Documents\waifu_moe\backend\target\debug\deps\ident_case-6491157a9dced91d.d: C:\Users\joaov\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ident_case-1.0.1\src\lib.rs
-
-C:\Users\joaov\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ident_case-1.0.1\src\lib.rs:
--- backend/app/target/debug/deps/vcpkg-43c8ea0c8b657747.d
+++
@@ -1,7 +0,0 @@
-C:\Users\joaov\Documents\waifu_moe\backend\target\debug\deps\libvcpkg-43c8ea0c8b657747.rmeta: C:\Users\joaov\.cargo\registry\src\index.crates.io-6f17d22bba15001f\vcpkg-0.2.15\src\lib.rs
-
-C:\Users\joaov\Documents\waifu_moe\backend\target\debug\deps\libvcpkg-43c8ea0c8b657747.rlib: C:\Users\joaov\.cargo\registry\src\index.crates.io-6f17d22bba15001f\vcpkg-0.2.15\src\lib.rs
-
-C:\Users\joaov\Documents\waifu_moe\backend\target\debug\deps\vcpkg-43c8ea0c8b657747.d: C:\Users\joaov\.cargo\registry\src\index.crates.io-6f17d22bba15001f\vcpkg-0.2.15\src\lib.rs
-
-C:\Users\joaov\.cargo\registry\src\index.crates.io-6f17d22bba15001f\vcpkg-0.2.15\src\lib.rs:
