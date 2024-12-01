737da67ee66920839eef1649c39c229f4a599193
RockyPher <davidsonjunior10@gmail.com>
2024-11-30 22:49:11 -0300

    chore: characters mock add

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,6 +1,8 @@
-// @ts-check
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  integrations: [svelte(), tailwind()],
+});
