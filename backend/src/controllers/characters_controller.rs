79804e90267aaa923c7911aeea37efd9f4f06fa7
JV-VM <joaovictoroliveiradourado2007@gmail.com>
2024-12-04 19:41:43 -0300

    chore: updating process from get characters

--- backend/src/controllers/characters_controller.rs
+++ backend/src/controllers/characters_controller.rs
@@ -5,7 +5,6 @@ use crate::services::characters_services;
 
 
 pub async fn get_characters(db: web::Data<DatabaseConnection>) -> impl Responder {
-    print!("hello");
     match characters_services::get_characters_service(&**db).await {
         Ok(characters) => {
             HttpResponse::Ok().json(characters)
