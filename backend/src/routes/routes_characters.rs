1105a304d7d01a8867052f2a05fde16699b34549
JV-VM <joaovictoroliveiradourado2007@gmail.com>
2024-12-05 00:35:25 -0300

    chore: continuando o backend

--- backend/src/routes/routes_characters.rs
+++ backend/src/routes/routes_characters.rs
@@ -4,7 +4,9 @@ use crate::controllers::characters_controller;
 
 pub fn init_characters_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::scope("/get_characters") // Base path
-            .route("", web::get().to(characters_controller::get_characters)),
+        web::scope("/characters") // Base path
+            .route("", web::get().to(characters_controller::get_characters))
+            //.route("", web::get().to(characters_controller::get_character))
+            ,
     );
 }
