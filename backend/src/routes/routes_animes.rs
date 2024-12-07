5a6d85333b52d92af7f9257986a26a58ab7d7fa0
JV-VM <joaovictoroliveiradourado2007@gmail.com>
2024-12-07 01:49:35 -0300

    chore: creating patch for likes

--- backend/src/routes/routes_animes.rs
+++ backend/src/routes/routes_animes.rs
@@ -1,12 +1,13 @@
 use actix_web::web::{self, route};
 
-use crate::controllers::{animes_controller, characters_controller};
+use crate::controllers::animes_controller;
 
 pub fn init_anime_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/animes")
             .route("", web::get().to(animes_controller::get_animes))
             .route("/{id}", web::get().to(animes_controller::get_anime))
-            .route("/{id}/characters", web::get().to(animes_controller::get_anime_character)),
+            .route("/{id}/characters", web::get().to(animes_controller::get_anime_character))
+            .route("/{id}/characters/{characters_name}", web::patch().to(animes_controller::patch_character_likes)),
     );
 }
--- backend/src/services/animes_services.rs
+++ backend/src/services/animes_services.rs
@@ -1,24 +1,47 @@
-use sea_orm::DatabaseConnection;
-
-use crate::repository::{anime_repo, character_repo};
+use sea_orm::{ActiveModelTrait, DatabaseConnection, Set};
 use crate::model::schema_animes::Model as Anime;
-use crate::model::schema_characters::Model as Characters;
+use crate::model::schema_characters::{ActiveModel, Model as Character};
+use crate::repository::{anime_repo, character_repo};
 
-pub async fn get_animes_services(
-    db: &DatabaseConnection,
-) -> Result<Vec<Anime>, sea_orm::DbErr> {
-    
+pub async fn get_animes_services(db: &DatabaseConnection) -> Result<Vec<Anime>, sea_orm::DbErr> {
     anime_repo::get_animes_repo(db).await
 }
-pub async fn get_anime_services(id: i64,
+pub async fn get_anime_services(
+    id: i64,
     db: &DatabaseConnection,
 ) -> Result<std::option::Option<Anime>, sea_orm::DbErr> {
-    anime_repo::get_anime_repo(id,db).await
+    anime_repo::get_anime_repo(id, db).await
 }
 pub async fn get_anime_character_service(
     id: i64,
     db: &DatabaseConnection,
-) -> Result<Vec<Characters>, sea_orm::DbErr> {
+) -> Result<Vec<Character>, sea_orm::DbErr> {
     print!("again");
-    character_repo::get_anime_character_repo(id,db).await
+    character_repo::get_anime_character_repo(id, db).await
+}
+pub async fn patch_character_likes_service(
+    id: i64,
+    name: String,
+    
+    db: &DatabaseConnection,
+) -> Result<Character, sea_orm::DbErr> {
+    let add_likes: i64 = 1;
+    match character_repo::patch_character_likes_repo(id, name.clone(), db).await {
+        Ok(Some(character)) => {
+            let mut active_character = ActiveModel::from(character);
+
+            // Update the number of likes
+            if let Some(current_likes) = active_character.num_likes.unwrap() {
+                active_character.num_likes = Set(Some(current_likes + add_likes));
+            } else {
+                active_character.num_likes = Set(Some(add_likes));
+            }
+
+            // Save the updated ActiveModel
+            let updated_character = active_character.update(db).await;
+            Ok(updated_character.unwrap())
+        }
+        Ok(None) => Err(sea_orm::DbErr::Custom(format!("Character '{}' not found", name))),
+        Err(err) => Err(err), // Propagate database error
+    }
 }
